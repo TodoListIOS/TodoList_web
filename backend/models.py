baa6a202c9cccbb41195b5fab8d067fb2526998f
cai rui <rui.cai2020@outlook.com>
2020-01-08 10:45:50 +0800

    add User details

--- backend/models.py
+++ backend/models.py
@@ -16,4 +16,12 @@ class Account(models.Model):
 
 # 用户记录表，用于记录用户备忘信息
 class UserRecords(models.Model):
-    pass
+    Email = models.EmailField(unique=True, default='user@empty.com', null=False, max_length=128)
+    timestamp = models.CharField(default='Tom_the_cat', null=False, max_length=128)
+    detail = models.TextField(default='Tom_the_cat', max_length=150)
+    checked = models.BooleanField(default=True)
+    sync = models.BooleanField(default=False)
+    due = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'UserRecords'
